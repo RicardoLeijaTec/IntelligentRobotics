7f8f9d39bcf7ceb26bcbefeb3bdc1c909d3e1c71
RicardoLeijaTec <a00828474@itesm.mx>
2022-04-26 09:59:15 -0500

    Major changes on location calculation
    Changes made to the correct angle function, removed some unnecessary functions, the reach goal function was rewritten but still has some tweaking to be made, and the conditionals for correcting angle and reach goal have a more stable way of functioning and probably will be on the final version.

--- location.py
+++ location.py
@@ -5,16 +5,13 @@ import time
 from math import pi
 import numpy as np
 from std_msgs.msg import Float32
-from std_msgs.msg import String
 from geometry_msgs.msg import Pose, Twist
-
 
 # Robot location
 xk = 0.0 # x position at k timestep in meters
 yk = 0.0 # y position at k timestep in meters
-thetak = 0.0 # angle at k timestep in radians
-'''	Values of theta must be contained within a single circle:
-	EITHER: -pi <= theta < pi OR: 0 <= theta < 2pi'''
+theta_k = 0.0 # angle at k timestep in radians #### theta r es igual a theta k
+'''	Values of theta must be contained within a single circle: EITHER: -pi <= theta < pi OR: 0 <= theta < 2pi'''
 
 # Robot constants
 r = 0.05 # Wheel radius, 0.05m
@@ -22,26 +19,17 @@ l = 0.19 # Distance between robot wheels 0.19m
 
 # Measured variables
 w_l, w_r = 0.0, 0.0 # Wheels velocity (rad/s)
-dt = 0.0052 # Time between samples (s)
+dt = 0.0055 # Time between samples (s) #0.00525 es preciso en fisico pero no en codigo # 0.0055 es preciso en codigo pero no en fisico
 
-final_pos_x = 0.0
+final_pos_x = 0.0 #Constante que nos da la posicion final del archivo
 final_pos_y = 0.0
 
-e_theta = 0.0
-theta_t = 0.0
-# theta r es igual a theta k
+e_theta = 0.07
 e_d = 0.0
 
-finished_change_Angle = False 
-isFinished = False
-goal_reached = False
+cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10, latch=True)
+angle_finished = 0
 distancia_recorrida = 0.0
-
-x = 0.0
-y = 0.0
-
-iteraciones = 0 
-iteraciones2 = 0 
 
 def getPos(posArray):
 	return odom_callback(posArray, 'final_pos')
@@ -54,116 +42,111 @@ def odom_l(wl):
 def odom_r(wr):
 	return odom_callback(wr, 'wr')
 
-def finished(x):
-	total = x + 1
-	if(total > 0):
-		return True 
-def finished2(x):
-	total = x + 1
-	if(total > 0):
-		return True 
 
+def correct_angle(error):
+	global cmd_vel_pub
+	w_to_cmd_vel = Twist()
+	vel_w = 0.08 * error
+	#print("vel w", vel_w, "Error: ", error)
+	if(-0.05 < error < 0.05):
+		w_to_cmd_vel.angular.z = 0.0
+		cmd_vel_pub.publish(w_to_cmd_vel)
+		#print("stopppp")
+		return True
+	if(error >= 0.8):
+		#print("gooooo")
+		w_to_cmd_vel.angular.z = vel_w
+		cmd_vel_pub.publish(w_to_cmd_vel)
+		return False
+		
 
+def reach_goal(distancia_total, distancia_faltante,distancia_recorrida):
+	global cmd_vel_pub
+	v_to_cmd_vel = Twist()
+	vel_v = 0.4
+	'''
+	if(-0.5 < distancia_faltante < 0.5):
+		print("STOPPPPPPP")
+		print("distancia faltante", distancia_faltante)
+		v_to_cmd_vel.linear.x = 0.0
+		cmd_vel_pub.publish(v_to_cmd_vel)
+		print(distancia_recorrida)
+		#return True
 
-pub_total = rospy.Publisher('cmd_vel', Twist, queue_size=10, latch=True)
-def correct_Angle(Actual, Deseado,iteraciones):
-	global pub_total
-	w_to_cmdVel  = Twist()
-	velo_W = 0.25*Deseado
-	print("Actual: ", Actual, "Deseado: ",Deseado, "velo_w: ", velo_W)
-
-	if(Actual > Deseado):
-		if(finished(iteraciones)):
-			return True 
-		print("once zero")
-		w_to_cmdVel.angular.z = 0.0
-		pub_total.publish(w_to_cmdVel)
+	if(distancia_faltante >= 0.8):
+		print("GOOOOOOOO")
+		v_to_cmd_vel.linear.x = vel_v
+		cmd_vel_pub.publish(v_to_cmd_vel)
+		print(distancia_recorrida)
+		return False'''
+		
+	distancia_recorrida =  distancia_total - distancia_faltante
+	print("Recorrida",distancia_recorrida, "Total", distancia_total, "Faltante", distancia_faltante)
+	error_distancia = distancia_total/2
+	if(distancia_recorrida < distancia_total-error_distancia):
+		print("GOOOOOOOO")
+		v_to_cmd_vel.linear.x = vel_v
+		cmd_vel_pub.publish(v_to_cmd_vel)
+		print(distancia_recorrida)
+		return False
+	if(distancia_recorrida >= distancia_total-error_distancia): 
+		print("STOPPPPPPP")
+		v_to_cmd_vel.linear.x = 0.0
+		cmd_vel_pub.publish(v_to_cmd_vel)
+		print(distancia_recorrida)
 		return True
 		
-	if(Actual < Deseado):
-		w_to_cmdVel.angular.z = velo_W
-		pub_total.publish(w_to_cmdVel) 
-		return False
-
-
-
-
-def reachGoal(distancia_total, distancia_faltante):
-	global distancia_recorrida, pub_total
-	
-	v_to_cmdVel = Twist()
-	velo_V = 0.05*distancia_faltante
-
-	distancia_recorrida = distancia_recorrida + (distancia_total - distancia_faltante)
-	print("=======================",distancia_recorrida, distancia_total)
-	
-        '''
-	if(velo_V > 13):
-		velo_V = 13
-	elif(velo_V < 1.5):
-		velo_V = 1.5'''
-	
-	if(distancia_recorrida >= distancia_total):
-                print("STOPPPPPPPPP")
-		v_to_cmdVel.linear.x = 0.0
-		pub_total.publish(v_to_cmdVel)
-		return True
-		
-	if(distancia_recorrida < distancia_total):
-		v_to_cmdVel.linear.x = velo_V
-		pub_total.publish(v_to_cmdVel)
-		return False
-
 
 def odom_callback(data, topic):
-
-	global xk, yk, thetak, degree
+	global xk, yk, theta_k, degree
 	global w_l, w_r
 	global final_pos_x, final_pos_y
-	global e_theta, theta_t
-	global iteraciones,iteraciones2
-
+	global e_theta
+	global distancia_recorrida, angle_finished
+	
 	if(topic == 'final_pos'):
 		final_pos_x = data.position.x
 		final_pos_y = data.position.y
-
+		e_theta = math.atan2(final_pos_y, final_pos_x) - theta_k
+	
 	if (topic == "wr"):
-		w_r = data.data 
+		w_r = data.data
+		
 	if (topic == "wl"):
 		w_l= data.data
 
-	thetak = thetak + r*((w_r - w_l)/l)*dt
-	degree = thetak*180/math.pi
-	#degree = thetak * 3.1416
+	theta_k = theta_k + r*((w_r - w_l)/l)*dt
+	degree = theta_k*180/math.pi
 
-	xk = (xk + r*((w_r + w_l)/2)*dt*math.cos(thetak))/1.9	
-	yk = (yk + r*((w_r + w_l)/2)*dt*math.sin(thetak))/1.9
-	
-	theta_t = math.atan2(final_pos_y, final_pos_x)
-	e_theta = theta_t - thetak
-
-	d_t = math.sqrt(  (final_pos_x)**2 + (final_pos_y)**2) # Distancia total por recorrer
+	xk = xk + (r*((w_r + w_l)/2)*dt*math.cos(theta_k))#/1.9
+	yk = yk + (r*((w_r + w_l)/2)*dt*math.sin(theta_k))#/1.9
+        
+	d_t = math.sqrt((final_pos_x)**2 + (final_pos_y)**2) # Distancia total por recorrer
 	e_d = math.sqrt((final_pos_x - xk)**2 + (final_pos_y - yk)**2)
-
-	if(thetak > math.pi):
-		thetak = -math.pi
-	if(thetak < -math.pi):
-		thetak = math.pi
+	
+	if(theta_k > 2*math.pi):
+		theta_k = 0
+	if(theta_k < 0):
+		theta_k = 2*math.pi
 	
 	print("Valor de x: ", xk)
 	print("Valor de y: ", yk)
-	print("Valor de theta: ", thetak)
+	#print("Valor de theta: ", theta_k)
 	print("Grados: ", degree)
-	print("Valor de e_theta: ", e_theta)
-	print("--------------------------------")
+	#print("Valor de e_theta: ", e_theta)
 	# print("Valor de ed: ", e_d)
-		
-	if(correct_Angle(thetak, e_theta,iteraciones) == True):
-		print("Angle Reached")
-		if(reachGoal(d_t, e_d) == True ):
-			print("Goal Reached ")
+	
+	if(angle_finished == 0):
+		#print("entro a ejecutar")
+		if(correct_angle(e_theta) == True):
+			angle_finished = 1
 
-	        
+	if(angle_finished == 1):
+		if(reach_goal(d_t , e_d, distancia_recorrida) == True):
+			angle_finished = 2
+			print("Se alcanzo el goal")
+
+		        
 def main():
 	rospy.init_node('prueba_localizacion_1p0')
 	r = rospy.Rate(100)
@@ -184,8 +167,7 @@ if __name__ == "__main__":
 # CALCULO CONSTANTE DE LA POSICION (no se si se reinicia cada que llama al main)    FUNCIONA
 # DAR SALIDA DE LOS 3 VALORES (prints de los valores)                               FUNCIONA
 # HACER QUE EL VALOR DE THETA OSCILE ENTRE LOS RANGOS DE VALORES DE THETA           FUNCIONA
-# PROBAR DIFERENTES dt                                                              FALLA
+# PROBAR DIFERENTES dt                                                              FUNCIONA PERO PUEDE MEJORAR
 # DEFINICION DE LAS VARIABLES FUERA DE LA FUNCION                                   FUNCIONA
 # PUBLICAR LOS VALORES (SOLO LOS TENEMOS CON PRINTS)                                POR HACER
 # 
-
